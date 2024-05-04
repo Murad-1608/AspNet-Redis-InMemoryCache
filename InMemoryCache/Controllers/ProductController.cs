416d13ff5455f35b945527d6468b0fc65b0ba9f5
Murad <murad.yunus.2017@gmail.com>
2024-05-04 19:48:55 +0400

    Simple changes

--- InMemoryCache/Controllers/ProductController.cs
+++ InMemoryCache/Controllers/ProductController.cs
@@ -13,6 +13,33 @@ namespace InMemoryCache.Controllers
 
         public IActionResult Index()
         {
+
+            if (!_memoryCache.TryGetValue<string>("Date", out string? dateCache))
+            {
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+                options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
+                options.Priority = CacheItemPriority.High;
+                options.RegisterPostEvictionCallback((key, value, reason, state) =>
+                {
+                    _memoryCache.Set<string>("callback", $"{key} --> {value} => Reason: {reason}");
+                });
+
+
+                _memoryCache.Set<string>("Date", DateTime.Now.ToString(), options);
+
+            }
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.Date = _memoryCache.Get<string>("Date");
+
+            _memoryCache.TryGetValue<string>("callback", out string? value);
+
+            ViewBag.callback = value;
+
             return View();
         }
     }
