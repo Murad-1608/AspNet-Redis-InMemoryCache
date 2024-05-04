7d8202a5b04ee590f0797c23a49ac8a6dfade7ac
Murad <murad.yunus.2017@gmail.com>
2024-05-04 20:47:23 +0400

    Simple changes

--- InMemoryCache/Controllers/ProductController.cs
+++ InMemoryCache/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InMemoryCache.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace InMemoryCache.Controllers
@@ -14,20 +15,29 @@ namespace InMemoryCache.Controllers
         public IActionResult Index()
         {
 
-            if (!_memoryCache.TryGetValue<string>("Date", out string? dateCache))
+            //if (!_memoryCache.TryGetValue<string>("Date", out string? dateCache))
+            //{
+            //    MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            //    options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
+            //    options.Priority = CacheItemPriority.High;
+            //    options.RegisterPostEvictionCallback((key, value, reason, state) =>
+            //    {
+            //        _memoryCache.Set<string>("callback", $"{key} --> {value} => Reason: {reason}");
+            //    });
+
+
+            //    _memoryCache.Set<string>("Date", DateTime.Now.ToString(), options);
+
+            //}
+
+            ProductModel productModel = new ProductModel
             {
-                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
-                options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
-                options.Priority = CacheItemPriority.High;
-                options.RegisterPostEvictionCallback((key, value, reason, state) =>
-                {
-                    _memoryCache.Set<string>("callback", $"{key} --> {value} => Reason: {reason}");
-                });
+                Id = 1,
+                Name = "Car",
+                Price = "35000"
+            };
 
-
-                _memoryCache.Set<string>("Date", DateTime.Now.ToString(), options);
-
-            }
+            _memoryCache.Set<ProductModel>("product", productModel);
 
             return View();
         }
@@ -39,6 +49,10 @@ namespace InMemoryCache.Controllers
             _memoryCache.TryGetValue<string>("callback", out string? value);
 
             ViewBag.callback = value;
+
+            _memoryCache.TryGetValue<ProductModel>("product", out ProductModel? product);
+
+            ViewBag.Product = product;
 
             return View();
         }
---
+++ InMemoryCache/Models/ProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InMemoryCache.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
