57e99de11ccd73832fc3f3643867bbd9b1626c9e
Murad <murad.yunus.2017@gmail.com>
2024-05-05 16:54:05 +0400

    Simple changes

---
+++ IDistributedCacheRedis/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedis.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+        public ProductController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            DistributedCacheEntryOptions options = new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(15)
+
+            };
+            _distributedCache.SetString("name", "Kamil", options);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            var value = _distributedCache.GetString("name");
+            ViewBag.Value = value;
+
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+
+            return View();
+        }
+
+    }
+}
