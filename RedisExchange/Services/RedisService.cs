15160ccb96d614833347e30e4492f01d66342fe4
Murad <murad.yunus.2017@gmail.com>
2024-05-05 21:13:03 +0400

    Simple changes

---
+++ RedisExchange/Services/RedisService.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+
+namespace RedisExchange.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+        private ConnectionMultiplexer _redis;
+        private IDatabase db;
+        public RedisService(IConfiguration configuration,
+                            ConnectionMultiplexer redis)
+        {
+            _redisHost = configuration["Redis:Host"]!;
+            _redisPort = configuration["Redis:Port"]!;
+            _redis = redis;
+        }
+
+        public void Connect()
+        {
+            string configString = $"{_redisHost}:{_redisPort}";
+
+            _redis = ConnectionMultiplexer.Connect(configString);
+        }
+
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
