3a481d4b5153e600048ca7d50d00af1c0a8f22f3
Murad <murad.yunus.2017@gmail.com>
2024-05-07 22:29:15 +0400

    Simple changes

--- RedisExchange/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RedisExchange.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
